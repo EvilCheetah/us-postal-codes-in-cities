419d2dce8cfac526ba31cc9bddcb7f8596289d0e
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-20 14:32:49 -0800

    refactor(): add other links

--- constant.py
+++ constant.py
@@ -1,1 +1,8 @@
-CONGRESSIONAL_DISTRICT_CODES_URL = 'https://www2.census.gov/geo/docs/reference/codes/files/national_cd113.txt'
+CENSUS_FTP_URI = 'ftp2.census.gov'
+
+
+CENSUS_DATA_URL = {
+    'POSTAL_CODES':   f'{CENSUS_FTP_URI}/geo/tiger/TIGER_RD18/LAYER/ZCTA520',
+    'PLACES':         f'{CENSUS_FTP_URI}/geo/tiger/TIGER_RD18/LAYER/PLACE',
+    'DISTRICT_CODES': f'{CENSUS_FTP_URI}geo/docs/reference/codes/files/national_cd113.txt'
+}
