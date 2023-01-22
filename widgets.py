d169e1835ca6ac1664265d260721c6228ca5faea
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-21 20:07:17 -0800

    add(): ftp-download progress bar widget

---
+++ widgets.py
@@ -0,0 +1,17 @@
+from progressbar import (
+    Bar,
+    ETA,
+    Percentage,
+    FileTransferSpeed,
+)
+
+from color import Color
+
+
+def ftp_download(filename: str):
+    return [
+        f'Downloading {Color.BOLD}{Color.GREEN}{filename}{Color.END}:', 
+        Percentage(), ' ',
+        Bar(marker = '#', left = '[', right = ']'), ' ',
+        ETA(), ' ', FileTransferSpeed()
+    ]
