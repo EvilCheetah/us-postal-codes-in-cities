20efea6725aa650f0bece9d8a6d6558dd9bcae44
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-21 17:45:39 -0800

    refactor(): move vars to the dedicated file

---
+++ census.py
@@ -0,0 +1,17 @@
+'''
+Entire file is dedicated to the values on US Census.gov website 
+'''
+
+FTP_URI = 'ftp2.census.gov'
+
+
+DATA_URL = {
+    'POSTAL_CODES':   '/geo/tiger/TIGER_RD18/LAYER/ZCTA520',
+    'PLACES':         '/geo/tiger/TIGER_RD18/LAYER/PLACE',
+    'DISTRICT_CODES': 'geo/docs/reference/codes/files'
+}
+
+
+FILE = {
+    'DISTRICT_CODES': 'national_cd113.txt'
+}
--- constant.py
+++
@@ -1,8 +0,0 @@
-CENSUS_FTP_URI = 'ftp://ftp2.census.gov'
-
-
-CENSUS_DATA_URL = {
-    'POSTAL_CODES':   f'{CENSUS_FTP_URI}/geo/tiger/TIGER_RD18/LAYER/ZCTA520',
-    'PLACES':         f'{CENSUS_FTP_URI}/geo/tiger/TIGER_RD18/LAYER/PLACE',
-    'DISTRICT_CODES': f'{CENSUS_FTP_URI}geo/docs/reference/codes/files/national_cd113.txt'
-}
