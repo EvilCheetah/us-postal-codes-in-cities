941e4b38e6adaa90c00354334ebc87d1a12f6aab
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-21 20:08:01 -0800

    add(): dedicated ftp-download function function also provides a progress bar

---
+++ utils.py
@@ -0,0 +1,42 @@
+from io import BufferedWriter
+from ftplib import FTP
+from pathlib import Path
+from progressbar import ProgressBar
+
+import widgets
+
+
+def ftp_download_file(
+    url:      str,
+    path:     str,
+    filename: str,
+    save_to:  Path
+) -> None:
+    with FTP( url ) as ftp:
+        ftp.login()
+        ftp.cwd( path )
+
+        with open( save_to, 'wb' ) as file:
+            _ftp_download_file_with_progress_bar(
+                ftp      = ftp,
+                filename = filename,
+                writer   = file
+            )
+
+
+def _ftp_download_file_with_progress_bar(
+    ftp: FTP,
+    filename: str,
+    writer: BufferedWriter,
+) -> int:
+    size = ftp.size( filename )
+
+    pbar = ProgressBar(widgets = widgets.ftp_download(filename), max_value = size)
+    pbar.start()
+
+    def write_file(data):
+        writer.write(data)
+        nonlocal pbar
+        pbar += len(data)
+
+    ftp.retrbinary(f'RETR {filename}', write_file)
