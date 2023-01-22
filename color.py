cc140c441d882b22f9b417fd504ea0a27770eef9
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-21 20:06:31 -0800

    add(): color pallete for terminal

---
+++ color.py
@@ -0,0 +1,14 @@
+class Color:
+    # Colors
+    RED       = '\033[91m'
+    YELLOW    = '\033[93m'
+    GREEN     = '\033[92m'
+    BLUE      = '\033[94m'
+    PURPLE    = '\033[95m'
+
+    # Emphasis
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    # End(escape) of Color/Emphasis
+    END       = '\033[0m'
