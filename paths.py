c536ec7677be1b5959c48ca7aecb24b03557575d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-23 09:03:40 -0800

    add(): places paths

--- paths.py
+++ paths.py
@@ -2,6 +2,12 @@ from pathlib import Path
 import census
 
 
-DISTRICT_CODES_DIR  = Path('in') / Path('districts')
+INPUT_DATA_FOLDER   = Path('in')
+
+DISTRICT_CODES_DIR  = INPUT_DATA_FOLDER / Path('districts')
 DISTRICT_CODES_FILE = Path( census.FILE['DISTRICT_CODES'] )
 DISTRICT_CODES_PATH = DISTRICT_CODES_DIR / DISTRICT_CODES_FILE
+
+PLACES_CODES_DIR    = INPUT_DATA_FOLDER / Path('places')
+PLACES_CODES_FILE   = lambda filename: (PLACES_CODES_DIR / Path(filename)) 
+PLACES_CODES_FILES  = PLACES_CODES_DIR.glob('*_place.zip')
