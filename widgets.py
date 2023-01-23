7b1d21c862334e8644d87953e6bd79e0ed3bf764
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-23 14:29:49 -0800

    fix(): text clutter on download progress

--- widgets.py
+++ widgets.py
@@ -10,7 +10,7 @@ from color import Color
 
 def ftp_download(filename: str):
     return [
-        f'Downloading {Color.BOLD}{Color.GREEN}{filename}{Color.END}:', 
+        f'Downloading {Color.BOLD}{Color.GREEN}{filename}{Color.END}: ', 
         Percentage(), ' ',
         Bar(marker = '#', left = '[', right = ']'), ' ',
         ETA(), ' ', FileTransferSpeed()
