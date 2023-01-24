5b3ad73bee1d3215a258378589369596a8a7638d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-24 14:37:11 -0800

    add(): saving data message refactor(): success message

--- logger.py
+++ logger.py
@@ -76,16 +76,17 @@ def LOAD_FILE_IN(filegroup: str) -> None:
     )
 
 
-def SUCCESSFUL_FILE_LOAD() -> None:
-    print(f'{Color.GREEN}{Color.BOLD}SUCCESS!{Color.END}')
-
-
-def SUCCESSFUL_FILE_SAVE(filegroup: str) -> None:
+def SAVING_DATA(filegroup: str) -> None:
     '''
     Structure
-        {filegroup} data was saved!
+        Saving {filegroup} data...
     '''
     print(
-        f'{Color.GREEN}{Color.BOLD}{filegroup}{Color.END}'
-        f' data was {Color.UNDERLINE}saved{Color.END}!'
+        f'Saving '
+        f'{Color.GREEN}{Color.BOLD}{filegroup}{Color.END} '
+        f'data... '
     )
+
+
+def SUCCESS_MESSAGE() -> None:
+    print(f'{Color.GREEN}{Color.BOLD}SUCCESS!{Color.END}')
