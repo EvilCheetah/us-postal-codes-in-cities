5551b8f45f3401bfce9eb6ffd5d1f0627fb7e79d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-24 09:50:11 -0800

    refactor(): using utils integrity check functions

--- districts.py
+++ districts.py
@@ -19,16 +19,16 @@ def get_district_codes(
     For some strange reason, header was shifted. Probably because
     default delimeter is r'\s'.
     '''
+    print( message.INITIATE_INTEGRITY_CHECK('District Codes') )
     utils.create_folder_if_not_exist( paths.DISTRICT_CODES_DIR )
-
-    if ( not (paths.DISTRICT_CODES_PATH).is_file() ):
-        print(message.MISSING_FILE('District Codes'))
-        utils.ftp_download_file(
-            url      = url,
-            path     = census.DATA_URL['DISTRICT_CODES'],
-            filename = census.FILE['DISTRICT_CODES'],
-            save_to  = paths.DISTRICT_CODES_PATH
-        )
+    utils.check_file_presence(
+        url       = url,
+        path      = census.DATA_URL['DISTRICT_CODES'],
+        filename  = census.FILE['DISTRICT_CODES'],
+        filegroup = 'District Codes',
+        save_to   = paths.DISTRICT_CODES_PATH
+    )
+    print( message.FINISH_INTEGRITY_CHECK('District Codes') )
     
     with open( paths.DISTRICT_CODES_PATH, 'r' ) as fin:
         data   = fin.readlines()
--- postal_codes.py
+++ postal_codes.py
@@ -13,16 +13,16 @@ def get_postal_codes_file(
         - `local storage` specified in 'paths.INPUT_DATA_FOLDER'
         - `Census` ftp-server
     '''
+    print( message.INITIATE_INTEGRITY_CHECK('Postal Codes') )
     utils.create_folder_if_not_exist( paths.POSTAL_CODES_DIR )
-
-    if ( not (paths.POSTAL_CODES_PATH).is_file() ):
-        print(message.MISSING_FILE('Postal Codes'))
-        utils.ftp_download_file(
-            url      = url,
-            path     = census.DATA_URL['POSTAL_CODES'],
-            filename = census.FILE['POSTAL_CODES'],
-            save_to  = paths.POSTAL_CODES_PATH
-        )
+    utils.check_file_presence(
+        url       = url,
+        path      = census.DATA_URL['POSTAL_CODES'],
+        filename  = census.FILE['POSTAL_CODES'],
+        filegroup = 'Postal Codes',
+        save_to   = paths.POSTAL_CODES_PATH
+    )
+    print( message.FINISH_INTEGRITY_CHECK('Postal Codes') )
     
     return paths.POSTAL_CODES_FILE
 
