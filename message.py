1ede133317f38716ee4935d21a1ec8802d73868f
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-24 08:29:02 -0800

    add(): corrupt file message

--- message.py
+++ message.py
@@ -13,3 +13,17 @@ def MISSING_FILE(filegroup: str):
        f'are {Color.RED}{Color.BOLD}MISSING{Color.END}\n'
        f'{Color.GREEN}{Color.BOLD}Downloading...{Color.END}'
     )
+
+
+def CORRUPT_FILE(filegroup: str):
+    '''
+    Structure:
+        One or more files in {filegroup} seems to be CORRUPTED\n
+        Re-downloading...
+    '''
+    return (
+       f'{Color.BOLD}One or more files{Color.END} in '
+       f'{Color.RED}{filegroup}{Color.END} '
+       f'seems to be {Color.RED}{Color.BOLD}CORRUPTED{Color.END}\n'
+       f'Re-downloading...'
+    )
