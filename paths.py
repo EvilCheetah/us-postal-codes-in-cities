999f00f651c46693db6618268e4f721e8cd4f296
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-24 13:01:59 -0800

    add(): out directory and path for postal code

--- paths.py
+++ paths.py
@@ -2,16 +2,19 @@ from pathlib import Path
 import census
 
 
-INPUT_DATA_FOLDER   = Path('in')
+INPUT_DATA_DIR        = Path('in')
+OUTPUT_DATA_DIR       = Path('out') 
 
-DISTRICT_CODES_DIR  = INPUT_DATA_FOLDER / Path('districts')
-DISTRICT_CODES_FILE = Path( census.FILE['DISTRICT_CODES'] )
-DISTRICT_CODES_PATH = DISTRICT_CODES_DIR / DISTRICT_CODES_FILE
+DISTRICT_CODES_DIR    = INPUT_DATA_DIR / Path('districts')
+DISTRICT_CODES_FILE   = Path( census.FILE['DISTRICT_CODES'] )
+DISTRICT_CODES_PATH   = DISTRICT_CODES_DIR / DISTRICT_CODES_FILE
 
-PLACES_DIR          = INPUT_DATA_FOLDER / Path('places')
-PLACES_FILE         = lambda filename: (PLACES_DIR / Path(filename)) 
-PLACES_FILES        = PLACES_DIR.glob('*_place.zip')
+PLACES_DIR            = INPUT_DATA_DIR / Path('places')
+PLACES_FILE           = lambda filename: (PLACES_DIR / Path(filename)) 
+PLACES_FILES          = PLACES_DIR.glob('*_place.zip')
 
-POSTAL_CODES_DIR    = INPUT_DATA_FOLDER / Path('postal-codes')
-POSTAL_CODES_FILE   = Path( census.FILE['POSTAL_CODES'] )
-POSTAL_CODES_PATH   = POSTAL_CODES_DIR / POSTAL_CODES_FILE
+POSTAL_CODES_DIR      = INPUT_DATA_DIR / Path('postal-codes')
+POSTAL_CODES_FILE     = Path( census.FILE['POSTAL_CODES'] )
+POSTAL_CODES_PATH     = POSTAL_CODES_DIR / POSTAL_CODES_FILE
+
+POSTAL_CODES_OUT_FILE = OUTPUT_DATA_DIR / Path('postal-codes.json')
