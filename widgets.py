8a654555e423967b3b45e5a0b84651de87cd6b04
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-24 09:39:45 -0800

    add(): integrity check widget

--- widgets.py
+++ widgets.py
@@ -10,8 +10,18 @@ from color import Color
 
 def ftp_download(filename: str):
     return [
-        f'Downloading {Color.BOLD}{Color.GREEN}{filename}{Color.END}: ', 
+        f'Downloading {Color.GREEN}{Color.BOLD}{filename}{Color.END}: ', 
         Percentage(), ' ',
         Bar(marker = '#', left = '[', right = ']'), ' ',
         ETA(), ' ', FileTransferSpeed()
+    ]
+
+
+def integrity_check(filegroup: str):
+    return [
+        f'Checking Integrity of '
+        f'{Color.GREEN}{Color.BOLD}{filegroup}{Color.END}: ',
+        Percentage(), ' ',
+        Bar(marker = '#', left = '[', right = ']'), ' ',
+        ETA()
     ]
