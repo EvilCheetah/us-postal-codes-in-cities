972ab1b5f4725f72eed3a26bcb3fe5af4d582e8f
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-01-26 11:17:00 -0800

    add(): places related paths

--- paths.py
+++ paths.py
@@ -18,3 +18,8 @@ POSTAL_CODES_FILE     = Path( census.FILE['POSTAL_CODES'] )
 POSTAL_CODES_PATH     = POSTAL_CODES_DIR / POSTAL_CODES_FILE
 
 POSTAL_CODES_OUT_FILE = OUTPUT_DATA_DIR / Path('postal-codes.json')
+
+PLACES_OUT_DIR        = OUTPUT_DATA_DIR / Path('places')
+PLACES_OUT_FILE       = lambda state_abbr: (
+    PLACES_OUT_DIR / Path(f'{state_abbr}_cities.json')
+)
